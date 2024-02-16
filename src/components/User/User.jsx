71e5d257421dfd5ba477a231e5ffe3d1d6d498fe
Om Shree <omshree@jmangroup.com>
2024-02-16 10:57:07 +0530

    fix: profile page

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./User.css";
 
 function User() {
-  const [userData, setuserData] = useState();
+  const [userData, setuserData] = useState([{}]);
   //send the data to backend
   var requestOptions = {
     method: "GET",
@@ -13,8 +13,8 @@ function User() {
     },
   };
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/UserDetail", requestOptions) //enter allTickets post end point.
+  useEffect( () => {
+ fetch("http://localhost:3000/api/UserDetail", requestOptions) //enter allTickets post end point.
       .then((response) => response.json())
       .then((result) => {
         console.log("response", result);
@@ -26,9 +26,21 @@ function User() {
   return (
     // looping all the user tickets to generate tickets booked
     /* Ticket container will contain all the tickets booked by the logged in user. */
-    <div className="Ticket-container">
-      {userData.map((user, index) => (
-        <div className="ticket" key={index}></div>
+    <div className="ticket-container">
+      {userData.map((user,index) => (
+        <div key={index}>
+        <div className="ticket-card">
+        <h1>{user.match_name}</h1>
+        <div className="second-row">
+          <h5>Venue: {user.venue}</h5>
+          <h5>Date: {user.date}</h5>
+        </div>
+
+        <h5 className="second-row">Seats no. {user.seat_no}</h5>
+        <h5 className="second-row">Booking Date: {user.payment_date}</h5>
+      </div>
+        </div>
+   
       ))}
     </div>
   );
