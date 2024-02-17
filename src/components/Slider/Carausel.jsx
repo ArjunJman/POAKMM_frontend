f96a7b62724397923bb7695a73c8641efb1de126
SHREEOM0611 <omshree.osj06@gmail.com>
2024-02-17 13:43:46 +0530

    added responsivity in homepage

--- src/components/Slider/Carausel.jsx
+++ src/components/Slider/Carausel.jsx
@@ -3,18 +3,18 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import "./carausel.css";
 import image1 from "../../images/slide-04.jpg";
-import image2 from "../../images/slide-05.jpg";
+import image2 from "../../images/slide-02.jpg";
 import image3 from "../../images/slide-06.jpg";
 
 // react-responsive carousel componet to get image slider
 function Carausel() {
   return (
-    <Carousel id="slider">
-      <div>
-        <img src={image2} />
-      </div>
+    <Carousel id="slider" showThumbs={false}>
       <div>
         <img src={image1} />
+      </div>
+      <div>
+        <img src={image2} />
       </div>
       <div>
         <img src={image3} />
